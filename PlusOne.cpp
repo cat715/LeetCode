57e5cf2257a61799ebe8b4a4cd6fc3047444b977
cat715 <bianguanlin007@gmail.com>
2014-09-07 22:42:49 +0800

    plus one

---
+++ PlusOne.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+      vector<int> plusOne(vector<int> &digits) {
+          add(digits, 1);
+          return digits;
+      }
+  private:
+      // 0 <= digit <= 9
+      void add(vector<int> &digits, int digit) {
+int c = digit; // carry, 进位
+          for_each(digits.rbegin(), digits.rend(), [&c](int &d){
+              d += c;
+c = d / 10;
+d %= 10; });
+          if (c > 0) digits.insert(digits.begin(), 1);
+      }
+};
